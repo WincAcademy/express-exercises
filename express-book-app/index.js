f4e03ff9ad3d619305e853c81b339cd2a627f209
Kristof Siket <kristofsiket@kinsta.com>
2023-06-26 08:10:39 +0200

    init: express-book-app

---
+++ express-book-app/index.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import booksRouter from './express-book-app/routes/books.js'
+import log from './express-book-app/middleware/logMiddleware.js'
+
+const app = express()
+app.use(express.json())
+
+app.use(log)
+
+app.use('/books', booksRouter)
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(3000, () => {
+    console.log('Server is listening on port 3000')
+})
