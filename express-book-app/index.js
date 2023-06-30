ec2bfaa9b60decf400a1442f1ae391734ef4e167
Mark van der Laan <mrkvdl@gmail.com>
2023-06-30 11:57:23 +0200

    add: record router to express-book-app

--- express-book-app/index.js
+++ express-book-app/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import booksRouter from './routes/books.js'
+import recordsRouter from './routes/records.js'
 import loginRouter from './routes/login.js'
 import log from './middleware/logMiddleware.js'
 import 'dotenv/config'
@@ -10,12 +11,13 @@ app.use(express.json())
 app.use(log)
 
 app.use('/books', booksRouter)
+app.use('/records', recordsRouter)
 app.use('/login', loginRouter)
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+  res.send('Hello World!')
 })
 
 app.listen(3000, () => {
-    console.log('Server is listening on port 3000')
+  console.log('Server is listening on port 3000')
 })
