5486d7bb6287dabc4e0ea40faeceb4c86eb18125
Kristof Siket <kristofsiket@kinsta.com>
2023-07-03 07:45:38 +0200

    feat: initialize new folder for auth0

---
+++ express-book-app-auth0/index.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import booksRouter from './routes/books.js'
+import loginRouter from './routes/login.js'
+import log from './middleware/logMiddleware.js'
+import 'dotenv/config'
+
+const app = express()
+app.use(express.json())
+
+app.use(log)
+
+app.use('/books', booksRouter)
+app.use('/login', loginRouter)
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(3000, () => {
+    console.log('Server is listening on port 3000')
+})
