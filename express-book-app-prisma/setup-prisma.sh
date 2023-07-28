a326b632c6e2fc34ab710739db695b9e492999f8
Kristof Siket <kristofsiket@kinsta.com>
2023-07-28 08:19:53 +0200

    feat: prisma version + eslint fix all

---
+++ express-book-app-prisma/setup-prisma.sh
@@ -0,0 +1,9 @@
+npm i prisma -D
+npx prisma init
+npx prisma format .
+npx prisma db push
+npm install @prisma/client
+npx prisma generate
+npx prisma db seed
+npx prisma studio
+npm run dev
--- express-book-app/routes/books.js
+++ express-book-app/routes/books.js
@@ -1,43 +1,43 @@
-import express from 'express';
-import getBooks from '../services/books/getBooks.js';
-import createBook from '../services/books/createBook.js';
-import getBookById from '../services/books/getBookById.js';
-import updateBookById from '../services/books/updateBookById.js';
-import deleteBook from '../services/books/deleteBook.js';
-import authMiddleware from '../middleware/auth.js';
-import notFoundErrorHandler from '../middleware/notFoundErrorHandler.js';
+import express from 'express'
+import getBooks from '../services/books/getBooks.js'
+import createBook from '../services/books/createBook.js'
+import getBookById from '../services/books/getBookById.js'
+import updateBookById from '../services/books/updateBookById.js'
+import deleteBook from '../services/books/deleteBook.js'
+import authMiddleware from '../middleware/auth.js'
+import notFoundErrorHandler from '../middleware/notFoundErrorHandler.js'
 
-const router = express.Router();
+const router = express.Router()
 
 router.get('/', (req, res) => {
-  const { genre, available } = req.query;
-  const books = getBooks(genre, available);
-  res.status(200).json(books);
-});
+  const { genre, available } = req.query
+  const books = getBooks(genre, available)
+  res.status(200).json(books)
+})
 
 router.post('/', authMiddleware, (req, res) => {
-  const { title, author, isbn, pages, available, genre } = req.body;
-  const newBook = createBook(title, author, isbn, pages, available, genre);
-  res.status(201).json(newBook);
-});
+  const { title, author, isbn, pages, available, genre } = req.body
+  const newBook = createBook(title, author, isbn, pages, available, genre)
+  res.status(201).json(newBook)
+})
 
 router.get(
   '/:id',
   (req, res) => {
-    const { id } = req.params;
-    const book = getBookById(id);
+    const { id } = req.params
+    const book = getBookById(id)
 
-    res.status(200).json(book);
+    res.status(200).json(book)
   },
   notFoundErrorHandler
-);
+)
 
 router.put(
   '/:id',
   authMiddleware,
   (req, res) => {
-    const { id } = req.params;
-    const { title, author, isbn, pages, available, genre } = req.body;
+    const { id } = req.params
+    const { title, author, isbn, pages, available, genre } = req.body
     const updatedBook = updateBookById(
       id,
       title,
@@ -46,24 +46,24 @@ router.put(
       pages,
       available,
       genre
-    );
-    res.status(200).json(updatedBook);
+    )
+    res.status(200).json(updatedBook)
   },
   notFoundErrorHandler
-);
+)
 
 router.delete(
   '/:id',
   authMiddleware,
   (req, res) => {
-    const { id } = req.params;
-    const deletedBookId = deleteBook(id);
+    const { id } = req.params
+    const deletedBookId = deleteBook(id)
 
     res.status(200).json({
       message: `Book with id ${deletedBookId} was deleted!`
-    });
+    })
   },
   notFoundErrorHandler
-);
+)
 
-export default router;
+export default router
